c1e45e9d179555d689f6708365edfecd1ccaa750
1-1-2 <50569812+1-1-2@users.noreply.github.com>
2024-07-12 12:46:35 +0800

    汉化界面，换国内源
    使用阿里云和HFmirror

--- app.py
+++ app.py
@@ -51,43 +51,42 @@ def _fn(path, solver, nfe, tau, chunk_seconds, chunks_overlap, denoising):
 
 def main():
     inputs: list = [
-        gr.Audio(type="filepath", label="Input Audio"),
-        gr.Dropdown(
-            choices=["Midpoint", "RK4", "Euler"], value="Midpoint", label="CFM ODE Solver (Midpoint is recommended)"
-        ),
+        gr.Audio(type="filepath", label="输入音频"),
+        gr.Dropdown(choices=["Midpoint", "RK4", "Euler"], value="Midpoint", label="CFM ODE 求解器（建议使用Midpoint）"),
         gr.Slider(
             minimum=1,
             maximum=128,
             value=64,
             step=1,
-            label="CFM Number of Function Evaluations (higher values in general yield better quality but may be slower)",
+            label="CFM 函数评估次数（通常值越高质量越好，但速度可能越慢）",
         ),
         gr.Slider(
             minimum=0,
             maximum=1,
             value=0.5,
             step=0.01,
-            label="CFM Prior Temperature (higher values can improve quality but can reduce stability)",
+            label="CFM 先验温度（较高的值可以提高质量，但可能降低稳定性）",
         ),
-        gr.Slider(minimum=1, maximum=40, value=10, step=1, label="Chunk seconds (more secods more VRAM usage)"),
-        gr.Slider(minimum=0, maximum=5, value=1, step=0.5, label="Chunk overlap"),
+        gr.Slider(minimum=1, maximum=40, value=10, step=1, label="单块秒数（单块时间越长，VRAM使用率越高）"),
+        gr.Slider(minimum=0, maximum=5, value=1, step=0.5, label="块间交叠"),
         # chunk_seconds, chunks_overlap
-        gr.Checkbox(
-            value=False, label="Denoise Before Enhancement (tick if your audio contains heavy background noise)"
-        ),
+        gr.Checkbox(value=False, label="增强前降噪（如果您的音频包含大量背景噪音，请勾选）"),
     ]
 
     outputs: list = [
-        gr.Audio(label="Output Denoised Audio"),
-        gr.Audio(label="Output Enhanced Audio"),
+        gr.Audio(label="输出降噪音频"),
+        gr.Audio(label="输出增强音频"),
     ]
 
     interface = gr.Interface(
         fn=_fn,
         title="Resemble Enhance",
-        description="AI-driven audio enhancement for your audio files, powered by Resemble AI.",
+        description="由Resemble AI驱动的人工智能音频增强，为您的音频文件提供动力。",
         inputs=inputs,
+        submit_btn="启动",
+        clear_btn="重置",
         outputs=outputs,
+        flagging_options=[("标记", "")],
     )
 
     interface.launch()
--- install.bat
+++ install.bat
@@ -3,7 +3,8 @@
 python -m venv venv
 call venv/scripts/activate
 
-pip install torch==2.3.1+cu118 torchaudio==2.3.1+cu118 --index-url https://download.pytorch.org/whl/torch_stable.html
+REM pip install -v torch==2.3.1+cu118 torchaudio==2.3.1+cu118 -f https://mirror.sjtu.edu.cn/pytorch-wheels/cu118
+pip install -v torch==2.3.1+cu118 torchaudio==2.3.1+cu118 -f https://mirrors.aliyun.com/pytorch-wheels/cu118
 pip install -r requirements.txt
 
 @echo Instaling deepspeed for python 3.10.x or 3.11 and CUDA 11.8
